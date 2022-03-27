b0620da0dee1401bacb7908275d957b873e062cd
daysond <daysond@outlook.com>
2022-03-27 16:18:40 -0400

    Initial Commit

---
+++ Assignment 2: The Weighted List Datatype/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment 2: The Weighted List Datatype
+//
+//  Created by Dayson Dong on 2022-03-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
