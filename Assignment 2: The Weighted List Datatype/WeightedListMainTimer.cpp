fa04afd70433117d90f3b44cf2ab9871a1caa96d
daysond <daysond@outlook.com>
2022-04-01 14:16:32 -0400

    added test file

--- Assignment 2: The Weighted List Datatype/WeightedListMainTimer.cpp
+++ Assignment 2: The Weighted List Datatype/WeightedListMainTimer.cpp
@@ -1,10 +1,11 @@
 //WeightedListMain.cpp - main program for testing the weighted list
 
+#include <fstream>
 #include "WeightedList.h"
 
 using namespace std;
 
-int main2() {
+int main() {
     asg::WeightedList<int> wList;
 
 	//Build up a list
@@ -13,6 +14,8 @@ int main2() {
 	wList.push_back(3);
 	wList.push_back(2);
 	wList.push_back(1);
+
+	cout << "Original list:" << endl;
 	for (auto e = wList.begin(); e != wList.end(); ++e) {
 		cout << *e << endl;
 	}
@@ -86,6 +89,12 @@ int main2() {
 	cout << endl;
 
 	//erase the entire list
+//	for (auto e = wList.begin(); e != wList.end(); ++e) {//MW: why does this not work?
+//		wList.erase(e);
+//	}
+//	for (auto e = wList.begin(); e != wList.end(); ) {//MW: why does this work?
+//		e = wList.erase(e);
+//	}
 	wList.erase(wList.begin(), wList.end());
 	cout << "erase the entire list:" << endl;
 	for (auto e = wList.begin(); e != wList.end(); ++e) {
@@ -132,7 +141,7 @@ int main2() {
 	cout << endl;
 
 	//Test the copy constructor
-    asg::WeightedList<int> wList2(wList);
+	asg::WeightedList<int> wList2(wList);
 	wList2.search(19);
 	cout << "create wList2 from wList by copy constructor with search 19:" << endl;
 	for (auto e = wList2.begin(); e != wList2.end(); ++e) {
@@ -144,7 +153,7 @@ int main2() {
 	cout << "wList2 empty:" << wList2.empty() << " wList2 size:" << wList2.size() << endl << endl;
 
 	//Test the copy assignment
-    asg::WeightedList<int> wList3 = wList;
+	asg::WeightedList<int> wList3 = wList;
 	wList3.search(18);
 	cout << "create wList3 from wList by copy assignment with search 18:" << endl;
 	for (auto e = wList3.begin(); e != wList3.end(); ++e) {
@@ -180,6 +189,45 @@ int main2() {
 	}
 	cout << endl;
 
+	//EXPERIMENT: Create a list of 10,000 data elements.
+    asg::WeightedList<int> expList;
+	const int LIST_SIZE = 10000;
+	const int DATA = 6;
+	cout << "creating a list of " << LIST_SIZE << " elements:" << endl;
+	for (int i = 0; i < LIST_SIZE; ++i) {
+		if(i!=DATA) expList.push_back(i);
+	}
+	//Now insert 6 at the end of this list
+	expList.push_back(DATA);
+
+	//Perform searches for 6 and measure the time for each search.
+	//We will search for two random numbers, then for 6 in order
+	//to simulate a realistic situation where 6 is often searched for.
+	const long nanosecsPerSecond=1000000000;
+	timespec startTime, endTime;
+	double totalTime;
+	const int NUM_SEARCHES = 2*LIST_SIZE;
+	//store the results to a file
+	ofstream outfile;
+	outfile.open("searchTime2.dat");
+	cout << "performing " << NUM_SEARCHES << " searches:" << endl;
+	for (int i = 0; i < NUM_SEARCHES; ++i) {
+		//search for two random numbers that are not equal to DATA (6)
+		int randomNumber;
+		for (int j = 0; j < 2; ++j) {
+			do {
+				randomNumber = rand() % LIST_SIZE;
+			} while (randomNumber == DATA);
+			expList.search(randomNumber);
+		}
+		//Now search for DATA and record the search time in milliseconds
+		clock_gettime(CLOCK_REALTIME, &startTime);
+		expList.search(DATA);
+		clock_gettime(CLOCK_REALTIME, &endTime);
+		totalTime = (double)(endTime.tv_sec-startTime.tv_sec)*nanosecsPerSecond + (endTime.tv_nsec-startTime.tv_nsec);
+		outfile << i << ", " << totalTime << endl;
+	}
+	outfile.close();
 
 	return 0;
 }
